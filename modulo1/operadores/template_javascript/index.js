e38940accaca49b3498da61885e9c4a3a2b46b57
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-02-10 10:42:32 -0300

    Entrega AulaOperadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,70 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO
+
+/* Exercício 1
+  Primeiro console.log: false
+  Segundo console.log: false
+  Terceiro console.log: true
+  Quarto console.log: boolean
+*/
+ 
+/* Exercício 2
+  Os números não serão somados e sim, adicionados um ao lado do outro. 
+*/
+
+/* Exercício 3
+  Sugiro ao meu colega que converta em número o valor recebido com a função Number. 
+  
+  let primeiroNumero = Number(prompt("Digite um numero!"))
+  let segundoNumero = Number(prompt("Digite outro numero!"))
+
+   const soma = primeiroNumero + segundoNumero
+
+   console.log(soma)
+*/
+
+
+// EXERCÍCIO DE ESCRITA
+
+/* Exercício 1
+let idade = Number(prompt("Qual a sua idade?"))
+let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo"))
+let diferenca = Number(idade) - Number(idadeAmigo)
+
+console.log("Sua idade é maior do que a do seu melhor amigo?" , idade > idadeAmigo)
+
+console.log("A diferença de idade entre vocês é de: " , diferenca , "anos")
+*/
+
+/* Exercício 2
+let numeroPar = Number(prompt("Insira um número par:"))
+let resto = numeroPar % 2
+
+console.log("O resto da divisão é: " , resto)
+
+// Padrão: Todo número par dividido por outro número par tem como resultado 0
+// Se inserir um número impar o resto será sempre 1
+*/
+
+/* Exercício 3
+let idadeAnos = Number(prompt("Qual a sua idade em anos? "))
+let idadeMeses = Number(idadeAnos) * 12
+let idadeDias = Number(idadeAnos) * 365
+let idadeHoras = Number(idadeAnos) * 8760
+
+console.log("Sua idade em meses é: " , idadeMeses)
+console.log("Sua idade em dias é: " , idadeDias)
+console.log("Sua idade em horas é:" , idadeHoras)
+*/
+
+/*Exercício 4
+let numero1 = Number(prompt("Informe um número: "))
+let numero2 = Number(prompt("Informe outro número: "))
+let primeiroNumeroDivisivel = numero1 > numero2
+let segundoNumeroDivisivel = numero2 > numero1
+
+console.log("O primeiro número é maior que o segundo?" , numero1 > numero2)
+console.log("O primeiro número é igual ao segundo?" , numero1 === numero2)
+console.log("O primeiro número é divisivel pelo segundo?" , primeiroNumeroDivisivel)
+console.log("O segundo número é divisível pelo primeiro?", segundoNumeroDivisivel)
+*/
+
