9ce223d56703807f42f98a896983c519fe6743b5
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-03-18 18:42:31 -0300

    Exercício 15A: check!

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -102,7 +102,7 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
@@ -137,7 +137,11 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+  consultas.sort(function(a,b) {
+    return (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0) ;
+});
+
+return consultas
 }
 
 // EXERCÍCIO 15B
