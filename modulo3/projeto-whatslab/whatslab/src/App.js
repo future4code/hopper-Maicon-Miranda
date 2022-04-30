ed95e16947f5ea18f8462b142917e21dacdfe425
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-04-29 22:15:55 -0300

    Entrega ProjetoWhatslab

---
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Estou completamente perdido em React, não consegui fazer nada. Estudando por fora para tentar enteder o conteúdo.</h1>
+    </div>
+  );
+}
+
+export default App;
