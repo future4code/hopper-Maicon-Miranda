dcf1afe6edb284543d9efeafb00746c12f329618
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-05-07 12:33:32 -0300

    Exercício atualizado

--- modulo3/jsx-labetube/react-labetube/src/App.js
+++ modulo3/jsx-labetube/react-labetube/src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -29,35 +29,35 @@ function App() {
             </nav>
 
             <section className="painel-de-videos">
-                <div className="box-pagina-principal media1" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media1" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=1 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media2" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media2" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=2 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media3" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media3" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=3 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media4" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media4" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=4 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media5" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media5" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=5 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media6" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media6" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=6 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media7" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media7" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=7 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
-                <div className="box-pagina-principal media8" onClick={reproduzVideo()}>
+                <div className="box-pagina-principal media8" onClick={reproduzVideo}>
                     <img src="https://picsum.photos/400/400?a=8 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
