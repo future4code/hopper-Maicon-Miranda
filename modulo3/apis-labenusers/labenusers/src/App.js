e82a0c7c479de74673e861d4a2f8cb449d835420
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-05-19 23:06:02 -0300

    Entrega aulaApisLabenusers

---
+++ modulo3/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import IntegracaoApi from './Components/IntegracaoApi'
+
+function App() {
+  return (
+
+    <div>
+      <IntegracaoApi />
+    </div>
+
+  )
+
+}
+export default App;
---
+++ modulo3/apis-labenusers/labenusers/src/Components/OutraTela.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+class OutraTela extends React.Component{
+    render(){
+        return
+    }
+}
