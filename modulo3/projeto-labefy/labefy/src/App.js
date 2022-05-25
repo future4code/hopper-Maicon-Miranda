a4fbd55b0b01f5066065f87ab81589d9f8e3df38
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-05-24 23:04:41 -0300

    Atualização do projeto

---
+++ modulo3/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AddPlaylist from './Components/AddPlaylist'
+
+function App() {
+  return (    
+    <div div className = "App">
+      <p></p>
+      <AddPlaylist/>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo3/projeto-labefy/labefy/src/Components/ListaPlaylist.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+
+class ListaPlaylist extends React.Component{
+ render(){
+        return
+    }
+}
+
+export default ListaPlaylist;
