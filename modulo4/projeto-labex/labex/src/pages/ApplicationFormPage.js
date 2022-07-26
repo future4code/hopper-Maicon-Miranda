812032be493dfad3acca16b13acdf2c2185452cb
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-07-26 19:17:53 -0300

    atualizações

--- modulo4/projeto-labex/labex/src/pages/ApplicationFormPage.js
+++ modulo4/projeto-labex/labex/src/pages/ApplicationFormPage.js
@@ -1,15 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const FormPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid;
+  margin-right: 30%;
+  margin-left: 30%;
+  margin-bottom: 1%;
+
+`
+
+const FormPageButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2%;
+`
 
 const ApplicationFormPage = () => {
   
   const navigate = useNavigate();
 
     return (
-      <div>
+      <FormPage>
         <h1>Inscreva-se para uma viagem</h1>
-        <button onClick={() => navigate("/trips/list")}>Voltar</button>
-        <button>Enviar</button>
-      </div>
+
+
+        <FormPageButtons>
+          <button onClick={() => navigate("/trips/list")}>Voltar</button>
+          <button>Enviar</button>
+        </FormPageButtons>
+      </FormPage>
     );
   };
   
