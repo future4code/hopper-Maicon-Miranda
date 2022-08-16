f02dfc807526ab751261065bc25e111c4d5de7d5
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-08-15 22:10:42 -0300

    Exercício 2 OK

--- modulo5/projeto-lista-ts/src/index.ts
+++ modulo5/projeto-lista-ts/src/index.ts
@@ -1,8 +1,23 @@
 // EXERCÍCIO 1
 
+/*
 const nome :string = process.argv[2]
 const dataNascimento :string = process.argv[3]
 
 const dataFormatada :Array<string> = dataNascimento.split("/")
 
 console.log(`Olá me chamo ${nome}, nasci no dia ${dataFormatada[0]} do mês de ${dataFormatada[1]} do ano de ${dataFormatada[2]}`)
+
+*/
+
+//EXERCÍCIO 2
+
+/*
+function tipoVariavel(input: any) :void {
+    console.log(typeof input)
+}
+
+tipoVariavel("Olá")
+
+*/
+
