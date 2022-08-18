95a17d2af1293d2d585f401ed9fd897cb01d72e6
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-08-18 19:57:11 -0300

    Exercício 1 ok

---
+++ modulo5/intro-express/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+})
+
+
+//EXERCíCIO 1
+
+/*
+
+app.get("/", (request :Request, response :Response) => {          
+    console.log("Tudo ok!")
+    response.send("Server is running in http://localhost:3003")
+})
+
+*/
