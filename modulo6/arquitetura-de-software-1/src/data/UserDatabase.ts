d6921156fbc5ada1901015e4552dae605dbd27c5
Maicon-Miranda <maicon.miranda77@hotmail.com>
2022-10-14 21:11:17 -0300

    Atualizado com endpoint createUser

---
+++ modulo6/arquitetura-de-software-1/src/data/UserDatabase.ts
@@ -0,0 +1,19 @@
+import { BaseDatabase } from "./BaseDatabase";
+import { user } from "../types/user";
+
+export class UserDatabase extends BaseDatabase {
+
+    public insertUser =async (user: user) => {
+        try {
+            await UserDatabase.connection.insert({
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                password: user.password
+               }).into('User_Arq')
+        } catch (error: any) {
+            throw new Error(error.message)
+        }
+       
+    }
+}
