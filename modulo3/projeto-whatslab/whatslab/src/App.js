98a49dc1f753fd29d4ba95794e6a8303fe14e612
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-03-01 20:33:59 -0300

    Projeto atualizado

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import './index.css'
+import sendImage from './assets/sendImage.png'
 
 export class App extends React.Component {
 
@@ -46,27 +47,39 @@ export class App extends React.Component {
   render() {
 
     const listaDeComponentes = this.state.chat.map((chat) => {
-      return (
-        <p><strong>{chat.usuario}</strong> {chat.mensagem}</p>
-      )
+      if (chat.usuario === "eu" || chat.usuario === "Eu" || chat.usuario === "EU") {
+        return (
+          <div className="balao-container-eu">
+            <p className="balao-mensagem-eu">{chat.mensagem}</p>
+          </div>
+        )} else if (chat.usuario) {
+          return (
+            <div className="balao-container-outro">
+              <p className="balao-mensagem-outro"><strong>{chat.usuario}: </strong> {chat.mensagem}</p>
+            </div>
+          )
+        }
     })
+
 
     return (
       <div className="container">
         <div className="box">
           <div className="box2">
-            <p>{listaDeComponentes}</p>
+            <p>{listaDeComponentes}</p>  
           </div>
           <div className="box3">
             <input
               placeholder="Usuário"
               className="inputUsuario"
+              type="text"
               value={this.state.usuarioEnviar}
               onChange={this.onChangeUsuario}
             />
             <input
-              placeholder="    Mensagem"
+              placeholder="Mensagem"
               className="inputMensagem"
+              type="text"
               value={this.state.mensagemEnviar}
               onChange={this.onChangeMensagem}
 
@@ -79,7 +92,7 @@ export class App extends React.Component {
               <div className="circle">
                 <i class="zmdi zmdi-mail-send">
                   <span class="material-symbols-outlined">
-
+                    <img src={sendImage} width='33'/>
                   </span>
                 </i>
               </div>
